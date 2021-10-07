b22b241375c1eac98756edffa4f74bf35097c80f
Pornpol Wasuwat <9pol@Pornpols-MacBook-Air.local>
2021-10-07 23:05:27 +0700

    init project

---
+++ src/Launcher.ts
@@ -0,0 +1,18 @@
+import { Server } from './Server/Server';
+
+class Launcher {
+  //instance variable
+  private name: string;
+  private server: Server;
+
+  constructor() {
+    this.server = new Server();
+  }
+
+  public launchApp() {
+    console.log('started app');
+    this.server.createServer();
+  }
+}
+
+new Launcher().launchApp();
---
+++ src/Server/Server.ts
@@ -0,0 +1,15 @@
+import { createServer, IncomingMessage, ServerResponse } from 'http'
+
+export class Server {
+  public createServer() {
+    createServer(
+      (req: IncomingMessage, res: ServerResponse) => {
+        console.log('got request from: ' + req.url);
+        res.end();
+      }
+    ).listen(8080)
+    console.log('server started');
+  }
+}
+
+export default Server;
